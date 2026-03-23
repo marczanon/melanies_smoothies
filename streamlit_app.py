d7bec2bc865ac5dfece75daf6633eb79c642c0a8
marczanon <59487782+marczanon@users.noreply.github.com>
2026-03-23 16:10:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ import requests
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.text)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
